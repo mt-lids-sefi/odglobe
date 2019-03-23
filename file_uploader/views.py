44d62f1b92e4b86e18e6c91434bff7e2dcc2d586
mar <tejedamarcia@gmail.com>
2019-03-23 17:41:37 -0300

    basic home

--- file_uploader/views.py
+++ file_uploader/views.py
@@ -1,3 +1,9 @@
+# Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    #Acá tengo que subir el archivo
+    #return HttpResponse("Hello, you're home.")
+    return render(request, 'home.html')
--- odg/urls.py
+++ odg/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Acá apunto a la home de subir archivo
+    path('file_uploader/', include('file_uploader.urls')),
 ]
+
