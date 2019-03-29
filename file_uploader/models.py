8b802daa87c3564f0a7328df3525a7f891e583eb
marcia-t <tejedamarcia@gmail.com>
2019-03-29 15:59:25 -0300

    adding name to documents

--- file_uploader/models.py
+++ file_uploader/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True, verbose_name='Descripción')
+    name = models.CharField(max_length=128, blank=True, verbose_name='Nombre')
     document = models.FileField(upload_to='files/', verbose_name='Archivo')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     document_id = models.AutoField(primary_key=True)
