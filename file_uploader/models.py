45f952a068272142c6f305a2e8571cd58de314c5
mar <tejedamarcia@gmail.com>
2019-04-05 00:34:31 -0300

    files extension

--- file_uploader/models.py
+++ file_uploader/models.py
@@ -5,8 +5,9 @@ from django.core.validators import FileExtensionValidator
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True, verbose_name='Descripción')
     name = models.CharField(max_length=128, blank=True, verbose_name='Nombre')
-    document = models.FileField(upload_to='files/', verbose_name='Archivo')
+    document = models.FileField(upload_to='files/', verbose_name='Archivo',  validators=[FileExtensionValidator(allowed_extensions=['csv'])])
     uploaded_at = models.DateTimeField(auto_now_add=True)
     document_id = models.AutoField(primary_key=True)
 
+#para agregar otra extensión simplemente hacerlo con ,'json' por ejemplo
 #,  validators=[FileExtensionValidator(allowed_extensions=['csv'])]
