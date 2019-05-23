1649f96e053be78f6f2bca6fa404b0e9d99090b3
marcia-t <tejedamarcia@gmail.com>
2019-05-23 16:32:10 -0300

    textarea description

--- file_uploader/forms.py
+++ file_uploader/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.forms import TextInput, Textarea
 from . import models
 
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = models.Document
         fields = ('name','description', 'document' )
+        widgets = {
+            'description': Textarea(attrs={'class': '???', 'rows': 5}),
+        }
