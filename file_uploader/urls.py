2ed7f4b8f72434fe7ecbc7e6f99d07ddbc23696c
mar <tejedamarcia@gmail.com>
2019-05-31 01:44:58 -0300

    refactor

--- file_uploader/urls.py
+++ file_uploader/urls.py
@@ -3,12 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-  #  path('su/', views.simple_upload, name='simple_upload'),
     path('mu/', views.model_form_upload, name='model_form_upload'),
-    path('gallery/', views.gallery, name='gallery'),
     path('documents/', views.DocumentsListView.as_view(), name='documents'),
     path('documents/<pk>/', views.document_detail, name='detail'),
-    path('documents/map/<pk>', views.document_map, name='map'),
+    #path('documents/map/<pk>', views.document_map, name='map'),
     path('documents/map_lf/<pk>', views.document_map_lf, name='map_lf'),
     path('document/<int:pk>/update', views.DocumentUpdateView.as_view(),name='document_update_form')
     #path('document/<int:pk>/cols', views.DocumentUpdateCols.as_view(),name='document_update_cols')
--- file_uploader/views.py
+++ file_uploader/views.py
@@ -23,16 +23,11 @@ def home(request):
 
 
 
-def gallery(request):
-    path='static'  # insert the path to your directory
-    img_list =os.listdir(path)
-    return render(request, 'gallery.html', {'images': img_list})
-
 def model_form_upload(request):
     if request.method == 'POST':
         form = forms.DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            doc =form.save()
+            form.save()
             #Paso a otra pantalla para seleccionar las columnas
             #return redirect('document_update_cols', pk=doc.document_id)
             return redirect('home')
@@ -40,6 +35,7 @@ def model_form_upload(request):
         form = forms.DocumentForm()
     return render(request, 'model_form_upload.html', {'form': form})
 
+#Ver el detalle del doc
 def document_detail(request, pk):
     #en pk viene el id del archivo
     document = get_object_or_404(Document, document_id=pk)
@@ -49,23 +45,7 @@ def document_detail(request, pk):
                 'data_html': data_html, 'route':document.document}
     return render(request, 'detail.html', context)
 
-def document_map(request,pk):
-    mimapa = folium.Map(location=[-34.641552, -58.479811])
-    document = get_object_or_404(Document, document_id=pk)
-    df = pd.read_csv(document.document)
-    df['longitude'] = df['longitude'].replace(r'\s+', np.nan, regex=True)
-    df['longitude'] = df['longitude'].replace(r'^$', np.nan, regex=True)
-    df['longitude'] = df['longitude'].fillna(-0.99999)
-    df['longitude'] = pd.to_numeric(df['longitude'])
-    df['latitude'] = df['latitude'].replace(r'\s+', np.nan, regex=True)
-    df['latitude'] = df['latitude'].replace(r'^$', np.nan, regex=True)
-    df['latitude'] = df['latitude'].fillna(-0.99999)
-    df['latitude'] = pd.to_numeric(df['latitude'])
 
-    FastMarkerCluster(data=list(zip(df['latitude'], df['longitude']))).add_to(mimapa)
-    m = mimapa._repr_html_()
-    context = { 'map': m , 'name': document.name, 'description': document.description, 'document': document}
-    return render(request, 'map.html', context)
 
 
 #Enviar el mapa a través de leaflet con js.
@@ -124,3 +104,29 @@ class DocumentUpdateView(UpdateView):
 #         #acá rendereo a otra pág
 #         return render(request, 'simple_upload.html', {'uploaded_file_url': uploaded_file_url, 'data_html': data_html})
 #     return render(request, 'simple_upload.html')
+
+#
+# def document_map(request,pk):
+#     mimapa = folium.Map(location=[-34.641552, -58.479811])
+#     document = get_object_or_404(Document, document_id=pk)
+#     df = pd.read_csv(document.document)
+#     df['longitude'] = df['longitude'].replace(r'\s+', np.nan, regex=True)
+#     df['longitude'] = df['longitude'].replace(r'^$', np.nan, regex=True)
+#     df['longitude'] = df['longitude'].fillna(-0.99999)
+#     df['longitude'] = pd.to_numeric(df['longitude'])
+#     df['latitude'] = df['latitude'].replace(r'\s+', np.nan, regex=True)
+#     df['latitude'] = df['latitude'].replace(r'^$', np.nan, regex=True)
+#     df['latitude'] = df['latitude'].fillna(-0.99999)
+#     df['latitude'] = pd.to_numeric(df['latitude'])
+#
+#     FastMarkerCluster(data=list(zip(df['latitude'], df['longitude']))).add_to(mimapa)
+#     m = mimapa._repr_html_()
+#     context = { 'map': m , 'name': document.name, 'description': document.description, 'document': document}
+#     return render(request, 'map.html', context)
+
+
+#
+# def gallery(request):
+#     path='static'  # insert the path to your directory
+#     img_list =os.listdir(path)
+#     return render(request, 'gallery.html', {'images': img_list})
